69db1d21e0b6ec724cb99af1f8f7129d086a6e38
Jia Li <60761935+jiali120@users.noreply.github.com>
2022-11-28 17:15:26 -0500

    Add files via upload

---
+++ Section one/HelloJava.java
@@ -0,0 +1,14 @@
+//class name should be same with file name
+//class name should use CamelCase
+public class HelloJava{
+    // main is the entry point
+    public static void main(String[] args){
+        //must have semi-colons ;
+        System.out.println("Hello Java");
+    }
+}
+
+// javac means Java Compiler
+
+//if using Common Terminal to run java code always compile code first and then run it.
+//so compile by using javac HelloJava.java  ------- and then run code by using java HelloJava
